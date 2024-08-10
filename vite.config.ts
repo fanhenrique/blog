2e5ef5efef09fdcbb5de232fe729bea547001f13
Henrique <fanhenrique@gmail.com>
2024-08-10 16:30:43 -0300

    config

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+
+  // plugins: [react(), mdPlugin({ mode: [Mode.HTML, Mode.MARKDOWN, Mode.REACT] })],
+  plugins: [react(), mdPlugin({ mode: [Mode.HTML, Mode.MARKDOWN, Mode.REACT] })],
+
+  // assetsInclude: ['markdown/']
+})
