fb6b3c7fb0894751ca2d53dc1b09499d7463045c
Henrique <fanhenrique@gmail.com>
2024-08-10 21:57:14 -0300

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+
+import markdownPlugin from './src/components/markdownPlugin'
+import colors from 'tailwindcss/colors'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        colors: colors,
+        'primary-color': '#0D80BF',
+        'secondary-color': '#161B22',
+        'backgroud-color': '#0F0F0F'
+      },
+      boxShadow: {
+        's': '10px 10px 10px -2px rgba(0, 0, 0, 0.3)',
+      }
+    },
+  },
+  plugins: [
+    markdownPlugin
+  ],
+}
+
