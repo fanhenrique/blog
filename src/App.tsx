610ab22b26f5756ad3bb6ba662da383a0c8a6a6e
Henrique <fanhenrique@gmail.com>
2024-08-13 23:44:54 -0300

    main

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+
+// internal imports
+import Router from './Router'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  )
+}
+
