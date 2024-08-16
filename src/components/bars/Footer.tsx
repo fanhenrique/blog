c301eaef0191b7ed2e11c88bea739030f25105f7
Henrique <fanhenrique@gmail.com>
2024-08-15 22:57:44 -0300

    bars components

---
+++ src/components/bars/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <div className="w-full h-32 flex flex-col items-center justify-center bg-secondary-color text-gray-200">
+            <h1 className="text-primary-color">blog</h1 >
+            <span>
+                © 2024. Todos os direitos reservados
+            </span>
+        </div >
+    )
+}
---
+++ src/components/bars/TopBar.tsx
@@ -0,0 +1,30 @@
+import { Sun } from "@phosphor-icons/react"
+import colors from "tailwindcss/colors"
+
+// internal import
+import ButtonMenu from "./ButtonMenu"
+import Search from "../Search"
+
+export default function TopBar() {
+
+    return (
+        <div className="sticky z-50 top-0 w-full h-20 flex items-center justify-center px-5 bg-secondary-color">
+            <div className="w-1/3">
+                <span className="text-primary-color text-2xl font-bold pointer-events-none">blog</span>
+            </div>
+            <div className="w-1/3 gap-x-14 flex justify-center">
+                <ButtonMenu navigate="/">Home</ButtonMenu>
+                <ButtonMenu navigate="/about">About</ButtonMenu>
+                <ButtonMenu navigate="/editors">Editores</ButtonMenu>
+                <ButtonMenu navigate="/contact">Contato</ButtonMenu>
+            </div>
+            <div className="w-1/3 gap-x-5 flex items-center justify-end">
+                <div className="flex flex-row">
+                    <Search placeholder="Pesquisar" />
+                </div>
+                <Sun size={28} color={colors.gray[200]} />
+            </div>
+
+        </div >
+    )
+}
