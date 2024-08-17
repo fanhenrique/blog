064b9749339d153e820b22dfe5166ca6ecf0c69a
Henrique <fanhenrique@gmail.com>
2024-08-16 23:21:54 -0300

    list authors

---
+++ src/components/ListAuthors.tsx
@@ -0,0 +1,27 @@
+interface propsListAuthors {
+    authors: string[] | undefined
+}
+
+export default function ListAuthors(props: propsListAuthors) {
+    return (
+        <div className="flex">
+            <span>Autor{props.authors ? props.authors.length > 1 ? 'es' : '' : <></>}:&nbsp;</span>
+            <div className="flex gap-x-2 flex-wrap font-semibold">
+                {props.authors?.map((author: string, i: number) => {
+                    return (
+                        <span
+                            key={author + i}
+                            className="
+                            hover:text-primary-color
+                            cursor-pointer
+                            hover:underline 
+                            underline-offset-2"
+                        >
+                            {author}
+                        </span>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
