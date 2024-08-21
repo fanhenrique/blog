df8d9d03099565a7165d8b1127787c0c4a885941
Henrique <fanhenrique@gmail.com>
2024-08-21 17:56:12 -0300

    button menu

--- src/components/bars/ButtonMenu.tsx
+++ src/components/bars/ButtonMenu.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { ButtonHTMLAttributes } from "react"
 
 export interface ButtonMenuProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -8,23 +8,24 @@ export interface ButtonMenuProps extends ButtonHTMLAttributes<HTMLButtonElement>
 export default function ButtonMenu(props: ButtonMenuProps) {
 
     const navigate = useNavigate()
+    const location = useLocation()
 
     return (
-        <button
-            {...props}
-            onClick={() => navigate(props.navigate)}
-            className="
+        <div className="w-full h-20 justify-center flex place-items-center">
+            <button
+                {...props}
+                onClick={() => { props.navigate == location.pathname ? navigate(0) : navigate(props.navigate) }}
+                className="
+                h-fit w-fit 
+                text-xl text-gray-200 font-semibold
                 outline-none 
-                text-xl 
-                text-gray-200 
-                font-semibold 
-                hover:border-b-4 
-                border-primary-color
-                focus:border-b-4
-                cursor-pointer
-            "
-        >
-            {props.children}
-        </button>
+                hover:underline focus:underline
+                decoration-primary-color
+                decoration-4 underline-offset-4
+                "
+            >
+                {props.children}
+            </button>
+        </div>
     )
 }
