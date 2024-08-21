eb0bd7fd0314cdf43297f920f67e1eeac2b24e26
Henrique <fanhenrique@gmail.com>
2024-08-21 16:03:25 -0300

    redirect to home

--- src/components/bars/Title.tsx
+++ src/components/bars/Title.tsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 interface TitleProps {
     textSize?: string
 }
 
 export default function Title({ textSize = 'text-3xl' }: TitleProps) {
+    const navigate = useNavigate()
     return (
         <span className={`
             ${textSize}
             text-primary-color 
             font-black 
-            pointer-events-none
+            cursor-pointer
             `}
+            onClick={() => navigate('/')}
         >
             blog
         </span >
