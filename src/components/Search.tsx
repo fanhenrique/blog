e953331a76d01ce090b8d629e6132e39fed7c36b
Henrique <fanhenrique@gmail.com>
2024-08-23 15:24:34 -0300

    search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,13 @@
 import { MagnifyingGlass } from "@phosphor-icons/react"
-import { useContext } from "react"
+import { InputHTMLAttributes, useContext } from "react"
 import colors from "tailwindcss/colors";
 
 // internal imports
 import { RefContext } from "./RefProvider";
 
+export interface SearchProps extends InputHTMLAttributes<HTMLInputElement> { }
 
-export default function Search() {
+export default function Search(props: SearchProps) {
 
     const context = useContext(RefContext);
 
@@ -18,8 +19,9 @@ export default function Search() {
     return (
         <div className="w-fit flex flex-row justify-end">
             <div className='flex items-center relative'>
-                <MagnifyingGlass className='absolute pl-1 pointer-events-none' size={28} weight="bold" color={colors.gray[500]} />
+                <MagnifyingGlass className='absolute pl-1 pointer-events-none' size={26} weight="bold" color={colors.gray[500]} />
                 <input
+                    {...props}
                     ref={context?.inputRef}
                     type='text'
                     name='search'
@@ -27,8 +29,8 @@ export default function Search() {
                     className={`
                         h-9 w-full
                         pl-7 text-xl
-                        border border-gray-700 rounded-md  
-                        bg-secondary-color text-gray-200       
+                        border border-gray-700 rounded-md
+                        bg-secondary-color text-gray-200
                         placeholder-gray-500 font-semibold
                         focus:outline-primary-color
                         focus:outline
