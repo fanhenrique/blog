1ce3647a751069c3f2a48e5ee0f2e4262d9f8b7e
Henrique <fanhenrique@gmail.com>
2024-08-29 15:47:16 -0300

    update hr

--- src/components/markdown/Hr.tsx
+++ src/components/markdown/Hr.tsx
@@ -2,6 +2,6 @@ import { HTMLAttributes } from "react";
 
 export default function Hr(props: HTMLAttributes<HTMLHRElement>) {
 
-    return <hr {...props} className="h-0.5 bg-gray-700 border-0" />
+    return <hr className="h-0.5 bg-gray-700 border-0" {...props} />
 
 }
