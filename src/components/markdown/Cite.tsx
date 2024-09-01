28bb442e87238b3bde764c7a0f630269020fd7f4
Henrique <fanhenrique@gmail.com>
2024-08-31 21:10:33 -0300

    update cite

--- src/components/markdown/Cite.tsx
+++ src/components/markdown/Cite.tsx
@@ -2,5 +2,5 @@ import { HTMLAttributes } from "react"
 
 export default function Cite(props: HTMLAttributes<HTMLElement>) {
 
-    return <div className="inline">&nbsp;[{props.children}]</div>
+    return <span className="text-sky-600">&nbsp;[{props.children}]</span>
 }
