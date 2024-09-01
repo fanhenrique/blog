9a598e49101ce5740f4b423554a7528a31c58990
Henrique <fanhenrique@gmail.com>
2024-09-01 17:40:53 -0300

    remove console log

--- src/components/cards/TagList.tsx
+++ src/components/cards/TagList.tsx
@@ -10,7 +10,6 @@ export default function TagList(props: TagListProps) {
     return (
         <div className="flex flex-row flex-wrap gap-x-1.5 pt-2  text-primary-color">
             {props.tags.map((tag: string, i: number) => {
-                console.log(tag)
                 return (
                     <Tag key={tag + i}>{tag}</Tag>
                 )
