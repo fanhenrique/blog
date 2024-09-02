c3e38ec3a7352a91234f35d1fbd602e99f4c88b9
Henrique <fanhenrique@gmail.com>
2024-09-02 11:44:27 -0300

    update tag

--- src/components/cards/Tag.tsx
+++ src/components/cards/Tag.tsx
@@ -1,4 +1,7 @@
-import { HTMLAttributes } from "react"
+import { HTMLAttributes, useContext } from "react"
+
+// Internal imports
+import { RefContext } from "../RefProvider"
 
 interface TagProps extends HTMLAttributes<HTMLSpanElement> {
     children: string
@@ -6,20 +9,22 @@ interface TagProps extends HTMLAttributes<HTMLSpanElement> {
 
 export default function Tag(props: TagProps) {
 
+    const context = useContext(RefContext)
+
     return (
-        <span
+        <button
             className="
                 hover:bg-primary-color
                 hover:text-gray-200
-                hover:cursor-pointer
                 text-nowrap
                 py-0.5 px-1 text-base
                 font-semibold
                 rounded-lg
             "
+            onClick={() => { context?.setInputValue(props.children) }}
             {...props}
         >
             {props.children}
-        </span>
+        </button>
     )
 }
