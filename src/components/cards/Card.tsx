8b339461bfe859da2bc476dceecdf2e10783f191
Henrique <fanhenrique@gmail.com>
2024-09-06 14:48:08 -0300

    card

---
+++ src/components/cards/Card.tsx
@@ -0,0 +1,19 @@
+interface CardProps {
+    children: JSX.Element
+    over: boolean
+}
+
+export default function Card(props: CardProps) {
+
+    return (
+        <div className={`
+            flex flex-col p-2 gap-y-2
+            text-gray-200
+            bg-secondary-color
+            shadow-s
+            ${props.over ? 'scale-[102%]' : ''}`}
+        >
+            {props.children}
+        </div>
+    )
+}
--- src/components/cards/PostCard.tsx
+++ src/components/cards/PostCard.tsx
@@ -5,45 +5,41 @@ import { AttributesPostInteface } from "../../pages/Post"
 import AuthorsList from "./AuthorsList";
 import TagList from "./TagList";
 import CardTitle from "./CardTitle";
+import Card from "./Card";
+import Hr from "../markdown/Hr";
 
 export default function PostCard(props: AttributesPostInteface) {
 
     const [over, setOver] = useState(false)
 
     return (
-        <div className={`
-            flex flex-col p-2 gap-y-2
-            text-gray-200 
-            bg-secondary-color 
-            shadow-s
-            ${over ? 'scale-[103%]' : ''}`}
-        >
-            <div className="flex flex-col gap-y-8">
+        <Card over={over}>
+            <>
+                <div className="flex flex-col gap-y-8">
 
-                <div className="w-full flex justify-between">
-                    <CardTitle
-                        over={over}
-                        onMouseOver={() => { setOver(true) }}
-                        onMouseOut={() => { setOver(false) }}
-                        navigate={`/post/${props.slug}`}
-                    >
-                        {props.title}
-                    </CardTitle>
+                    <div className="w-full flex justify-between">
+                        <CardTitle
+                            over={over}
+                            onMouseOver={() => { setOver(true) }}
+                            onMouseOut={() => { setOver(false) }}
+                            navigate={`/post/${props.slug}`}
+                        >
+                            {props.title}
+                        </CardTitle>
 
-                    <span className="w-fit h-fit text-nowrap text-gray-200">
-                        {props.date.toLocaleDateString("pt-BR")}
-                    </span>
+                        <span className="w-fit h-fit text-gray-200">
+                            {props.date.toLocaleDateString("pt-BR")}
+                        </span>
+                    </div>
+
+                    <AuthorsList authors={props.authors} />
+
                 </div>
 
-                <AuthorsList authors={props.authors} />
+                <Hr />
 
-            </div>
-
-            <hr className="h-px bg-gray-700 border-0 " />
-
-            <TagList tags={props.tags} />
-
-        </div >
-
+                <TagList tags={props.tags} />
+            </>
+        </Card>
     )
 }
