6b1931169781f29e25c257576f9191912410104f
Henrique <fanhenrique@gmail.com>
2024-09-07 17:02:27 -0300

    author card

---
+++ src/components/cards/AuthorCard.tsx
@@ -0,0 +1,71 @@
+import { useState } from "react"
+import { UserCircle } from "@phosphor-icons/react"
+
+import { AuthorsAttributesInterface } from "../../pages/Author"
+import TagList from "./TagList"
+import CardTitle from "./CardTitle"
+import Card from "./Card"
+import Hr from "../markdown/Hr"
+import ButtonSearch from "../ButtonSearch"
+import colors from "tailwindcss/colors"
+
+
+export default function AuthorCard(props: AuthorsAttributesInterface) {
+
+    const [over, setOver] = useState(false)
+    console.log(props.foto)
+
+    return (
+        <Card over={over}>
+            <>
+                <div className="flex flex-col">
+
+                    <div className="flex gap-x-4">
+                        <div className="w-1/5">
+                            {props.foto ?
+                                <img className="w-full rounded-full" src={props.foto} />
+                                :
+                                <UserCircle
+                                    className="size-full"
+                                    viewBox="25 25 205 205"
+                                    color={colors.gray[700]}
+                                    weight="thin"
+                                />
+                            }
+                        </div>
+                        <div className="w-full flex flex-col justify-between">
+                            <CardTitle
+                                over={over}
+                                onMouseOver={() => { setOver(true) }}
+                                onMouseOut={() => { setOver(false) }}
+                                navigate={`/author/${props.slug}`}
+                            >
+                                {props.author}
+                            </CardTitle>
+                            <div className="w-full flex justify-end">
+                                <ButtonSearch
+                                    search={props.author}
+                                    className="
+                                    w-fit
+                                    hover:text-primary-color
+                                    cursor-pointer
+                                    hover:underline 
+                                    underline-offset-2
+                                    font-semibold 
+                                "
+                                >
+                                    Postagens do autor
+                                </ButtonSearch>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <Hr />
+
+                <TagList tags={props.tags} />
+            </>
+        </Card >
+    )
+}
