a88b326147ba0be611a56d934b13ecc23314966c
Henrique <fanhenrique@gmail.com>
2024-09-08 15:05:00 -0300

    update post header

--- src/components/post/PostHeader.tsx
+++ src/components/post/PostHeader.tsx
@@ -10,8 +10,8 @@ interface PostHeaderProps {
 export default function PostHeader(props: PostHeaderProps) {
 
     return (
-        <div className='w-full flex flex-col gap-1'>
-            <div className='w-full flex justify-between text-gray-200'>
+        <div className='w-full flex flex-col gap-y-1'>
+            <div className='w-full flex justify-between'>
                 <AuthorsList authors={props.authors} />
                 <span className='text-nowrap text-gray-200'>
                     {props.date?.toLocaleDateString("pt-BR")}
