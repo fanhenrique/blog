497e84c01a1a93538c49ea18c7c5dd4dbbc935ed
Henrique <fanhenrique@gmail.com>
2024-09-08 22:33:19 -0300

    update router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,20 +2,22 @@ import { Routes, Route, Navigate } from "react-router-dom"
 
 // internal imports
 import Home from "./pages/Home"
-import About from "./pages/About"
 import Authors from "./pages/Authors"
 import Post from "./pages/Post"
 import RefProvider from "./components/RefProvider"
+import Author from "./pages/Author"
 
 export default function Router() {
 
     return (
-        <Routes>
-            <Route path='/' element={<RefProvider children={<Home />} />} />
-            <Route path='/post/:slug' element={<Post />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/authors' element={<Authors />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes >
+        <RefProvider>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/post/:slug' element={<Post />} />
+                <Route path='/authors' element={<Authors />} />
+                <Route path='/author/:slug' element={<Author />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes >
+        </RefProvider>
     )
 }
