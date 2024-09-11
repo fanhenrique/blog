10c87709702476769eb1bcc7c9fe8348539fc897
Henrique <fanhenrique@gmail.com>
2024-09-11 14:54:40 -0300

    update title

--- src/components/bars/Title.tsx
+++ src/components/bars/Title.tsx
@@ -1,19 +1,32 @@
+import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
+
+// Internal imports
+import { RefContext } from "../RefProvider"
 
 interface TitleProps {
     textSize?: string
 }
 
 export default function Title({ textSize = 'text-3xl' }: TitleProps) {
+
+    const context = useContext(RefContext)
     const navigate = useNavigate()
+
+    const redirect = () => {
+        context?.setInputValue('')
+        navigate('/')
+    }
+
     return (
-        <span className={`
-            ${textSize}
-            text-primary-color 
-            font-black 
-            cursor-pointer
+        <span
+            className={`
+                ${textSize}
+                text-primary-color 
+                font-black 
+                cursor-pointer
             `}
-            onClick={() => navigate('/')}
+            onClick={redirect}
         >
             blog
         </span >
