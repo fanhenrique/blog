9a645df314bc96087e462d32026326b7b9582f69
Henrique <fanhenrique@gmail.com>
2024-09-14 18:35:53 -0300

    list inside

--- src/components/markdown/Ol.tsx
+++ src/components/markdown/Ol.tsx
@@ -5,11 +5,12 @@ export default function Ol(props: OlHTMLAttributes<HTMLOListElement>) {
     return (
         <ol
             className="
-                w-full pl-14 
+                w-full pl-10
                 flex flex-col
                 mt-2 mb-5
                 gap-y-5
                 list-decimal
+                list-inside
                 text-gray-200
             "
         >
--- src/components/markdown/Ul.tsx
+++ src/components/markdown/Ul.tsx
@@ -1,14 +1,16 @@
 import { HTMLAttributes } from "react";
 
 export default function Ul(props: HTMLAttributes<HTMLUListElement>) {
+
     return (
         <ul
             className="
-                w-full pl-14 
+                w-full pl-10
                 flex flex-col
                 mt-2 mb-5
                 gap-y-5
                 list-disc
+                list-inside
                 text-gray-200
             "
         >
