226e69a971b1993ca38721c7c0947b0520ccbeb8
Henrique <fanhenrique@gmail.com>
2024-09-15 17:44:11 -0300

    list outside

--- src/components/markdown/Ol.tsx
+++ src/components/markdown/Ol.tsx
@@ -5,12 +5,12 @@ export default function Ol(props: OlHTMLAttributes<HTMLOListElement>) {
     return (
         <ol
             className="
-                w-full pl-10
+                w-full pl-14
                 flex flex-col
                 mt-2 mb-5
                 gap-y-5
                 list-decimal
-                list-inside
+                list-outside
                 text-gray-200
             "
         >
--- src/components/markdown/Ul.tsx
+++ src/components/markdown/Ul.tsx
@@ -5,12 +5,12 @@ export default function Ul(props: HTMLAttributes<HTMLUListElement>) {
     return (
         <ul
             className="
-                w-full pl-10
+                w-full pl-14
                 flex flex-col
                 mt-2 mb-5
                 gap-y-5
                 list-disc
-                list-inside
+                list-outside
                 text-gray-200
             "
         >
