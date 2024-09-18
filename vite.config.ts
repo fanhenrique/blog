2400e70865f101b22d8efb4053a486eb4cac4b4d
Henrique <fanhenrique@gmail.com>
2024-09-18 14:14:56 -0300

    remove vite-plugin-markdown

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), mdPlugin({ mode: [Mode.HTML, Mode.MARKDOWN, Mode.REACT] })],
+  plugins: [react()],
 })
