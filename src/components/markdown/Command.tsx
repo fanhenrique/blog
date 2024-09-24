3a996146054bd38638c62f39b116a703533f4bd2
Henrique Fan <fanhenrique@gmail.com>
2024-09-24 11:26:54 -0300

    command

--- src/components/markdown/Command.tsx
+++ src/components/markdown/Command.tsx
@@ -1,17 +1,23 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes } from 'react';
 
-export default function Command(props: { children: ReactNode }) {
+// Internal imports
+import CodeCopyButton from './CodeCopyButton';
+
+export default function Command(props: HTMLAttributes<HTMLPreElement>) {
 
     return (
-        <pre
-            className='
-                w-fit max-w-full
-                text-gray-200 font-semibold 
-                text-lg text-lef
-                whitespace-pre-wrap
-            '
-        >
-            {props.children}
-        </pre>
+        <div className="w-full flex p-2 justify-between bg-backgroud-color my-5">
+            <pre
+                className='
+                    w-fit max-w-full
+                    text-gray-200 font-semibold 
+                    text-lg text-lef
+                    whitespace-pre-wrap
+                '
+            >
+                {props.children}
+            </pre>
+            <CodeCopyButton>{props.children}</CodeCopyButton>
+        </div>
     )
 }
