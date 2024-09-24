4efc438d03eee282c2908f85915f5b56aebcb60a
Henrique Fan <fanhenrique@gmail.com>
2024-09-23 21:07:31 -0300

    mark component

---
+++ src/components/markdown/Mark.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes } from "react";
+
+export default function(props: HTMLAttributes<HTMLElement>){
+    return(
+        <mark 
+            className="
+                px-0.5
+                font-semibold
+                text-gray-200
+                bg-assistant-color
+            "
+        >
+            {props.children}
+        </mark>
+    )
+}
