0b28ad1124ae7226ca3b5040f63a6455173f4a50
Henrique Fan <fanhenrique@gmail.com>
2024-09-24 11:26:34 -0300

    code

--- src/components/markdown/Code.tsx
+++ src/components/markdown/Code.tsx
@@ -1,36 +1,19 @@
-import { HTMLAttributes } from 'react';
+import { HTMLAttributes } from "react";
 
-// internal imports
-import Command from './Command';
-import LanguageSyntax from './LanguageSyntax';
-import CodeCopyButton from './CodeCopyButton';
-
-export default function Code(props: HTMLAttributes<HTMLElement>) {
-
-    const match = /language-(\w+)/.exec(props.className || '')
+export default function Code(props: HTMLAttributes<HTMLElement>){
 
     return (
-        <>
-            {match ?
-                <div className="w-full flex p-2 justify-between bg-backgroud-color my-5">
-                    {match[1] === 'command' ?
-                        <Command>{props.children}</Command> : <LanguageSyntax language={match[1]}>{props.children}</LanguageSyntax>
-                    }
-                    <CodeCopyButton>{props.children}</CodeCopyButton>
-                </div>
-                :
-                <code
-                    className='
-                        w-fit inline
-                        text-gray-200 font-semibold 
-                        text-lg text-left
-                        bg-backgroud-color
-                        py-0.5 px-1.5 rounded-md
-                    '
-                >
-                    {props.children}
-                </code>
-            }
-        </ >
+        <code
+            className="
+                w-fit inline
+                text-gray-200 
+                text-lg
+                font-semibold 
+                bg-backgroud-color
+                py-0.5 px-1 rounded-md
+            "
+        >
+            {props.children}
+        </code>
     )
 }
