1537cdc0671a839302e20eaf8bb14eb965d2b568
Henrique Fan <fanhenrique@gmail.com>
2024-09-27 18:02:36 -0300

    reference section

---
+++ src/components/markdown/ReferenceSection.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes } from "react";
+
+export default function (props: HTMLAttributes<HTMLDivElement>) {
+    return (
+        <div
+            {...props}
+            className="
+                flex flex-col 
+                gap-y-5 
+                mt-2 mb-5
+            "
+        >
+            {props.children}
+        </div>
+    )
+}
